457214eed7c988dde4446a677098fc904a8211c0
chaechae922 <cele9222@naver.com>
2025-05-16 18:02:10 +0900

    RR

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,107 @@
 #include <Arduino.h>
 #include <PinChangeInterrupt.h>
 
+// 채널, 핀 정의 
 const int CH7_PIN = A0;
-const int CH3_PIN = A1;
-const int CH2_PIN = A2;
-
+const int CH3_PIN = A1;  
+const int CH2_PIN = A2; 
 const int LED_ONOFF_PIN  = 2;
 const int LED_BRIGHT_PIN = 3;
 const int LED_R_PIN      = 4;
 const int LED_G_PIN      = 5;
 const int LED_B_PIN      = 6;
 
+// 전역(인터럽트 공유) 변수
 volatile unsigned long rise7 = 0, rise3 = 0, rise2 = 0;
-volatile unsigned int  w7 = 1500, w3 = 1500, w2 = 1500;
-volatile bool          ch7_upd = false, ch3_upd = false, ch2_upd = false;
+volatile unsigned int  pw7   = 1500, pw3   = 1500, pw2   = 1500;
+volatile bool          up7   = false, up3   = false, up2   = false;
 
-void ISR_ch7() {
-  if (digitalRead(CH7_PIN)) {
-    rise7 = micros();
+// 인터럽트(ISR)
+inline void capturePulse(volatile unsigned long &rise,
+                         volatile unsigned int &pw,
+                         volatile bool &flag,
+                         int pin)
+{
+  if (digitalRead(pin)) {
+    rise = micros();                 // 상승
   } else {
-    unsigned long dt = micros() - rise7;
-    w7 = (dt >= 1000 && dt <= 2000) ? dt : 1500;
-    ch7_upd = true;
+    unsigned long dt = micros() - rise;
+    pw   = (dt >= 1000 && dt <= 2000) ? dt : 1500;
+    flag = true;                     // 새 값 표시
   }
 }
 
-void ISR_ch3() {
-  if (digitalRead(CH3_PIN)) {
-    rise3 = micros();
-  } else {
-    unsigned long dt = micros() - rise3;
-    w3 = (dt >= 1000 && dt <= 2000) ? dt : 1500;
-    ch3_upd = true;
-  }
-}
+void ISR_ch7() { capturePulse(rise7, pw7, up7, CH7_PIN); }
+void ISR_ch3() { capturePulse(rise3, pw3, up3, CH3_PIN); }
+void ISR_ch2() { capturePulse(rise2, pw2, up2, CH2_PIN); }
 
-void ISR_ch2() {
-  if (digitalRead(CH2_PIN)) {
-    rise2 = micros();
-  } else {
-    unsigned long dt = micros() - rise2;
-    w2 = (dt >= 1000 && dt <= 2000) ? dt : 1500;
-    ch2_upd = true;
-  }
-}
-
-void hsvToRgb(float h, float s, float v, int &r, int &g, int &b) {
+// HSV -> RGB 변환
+void hsvToRgb(float h, float s, float v, int &r, int &g, int &b)
+{
   float c = v * s;
-  float x = c * (1 - fabs(fmod(h / 60.0, 2) - 1));
+  float x = c * (1 - fabs(fmod(h / 60.0f, 2) - 1));
   float m = v - c;
   float r1, g1, b1;
+
   if      (h <  60) { r1 = c;  g1 = x;  b1 = 0; }
   else if (h < 120) { r1 = x;  g1 = c;  b1 = 0; }
   else if (h < 180) { r1 = 0;  g1 = c;  b1 = x; }
   else if (h < 240) { r1 = 0;  g1 = x;  b1 = c; }
   else if (h < 300) { r1 = x;  g1 = 0;  b1 = c; }
   else              { r1 = c;  g1 = 0;  b1 = x; }
+
   r = (r1 + m) * 255;
   g = (g1 + m) * 255;
   b = (b1 + m) * 255;
 }
 
-void setup() {
+
+// 초기 설정
+void setup()
+{
   pinMode(CH7_PIN, INPUT_PULLUP);
   pinMode(CH3_PIN, INPUT_PULLUP);
   pinMode(CH2_PIN, INPUT_PULLUP);
+
   pinMode(LED_ONOFF_PIN,  OUTPUT);
   pinMode(LED_BRIGHT_PIN, OUTPUT);
   pinMode(LED_R_PIN,      OUTPUT);
   pinMode(LED_G_PIN,      OUTPUT);
   pinMode(LED_B_PIN,      OUTPUT);
+
   attachPCINT(digitalPinToPCINT(CH7_PIN), ISR_ch7, CHANGE);
   attachPCINT(digitalPinToPCINT(CH3_PIN), ISR_ch3, CHANGE);
   attachPCINT(digitalPinToPCINT(CH2_PIN), ISR_ch2, CHANGE);
+
   Serial.begin(115200);
 }
 
-void loop() {
+// 메인 루프
+void loop()
+{
+  // 1) 인터럽트 값 스냅샷
   noInterrupts();
-  unsigned int pw7 = w7;
-  unsigned int pw3 = w3;
-  unsigned int pw2 = w2;
-  bool u7 = ch7_upd, u3 = ch3_upd, u2 = ch2_upd;
-  ch7_upd = ch3_upd = ch2_upd = false;
+    unsigned int p7 = pw7;
+    unsigned int p3 = pw3;
+    unsigned int p2 = pw2;
+    up7 = up3 = up2 = false;
   interrupts();
 
-  digitalWrite(LED_ONOFF_PIN, pw7 > 1500 ? HIGH : LOW);
+  // 2) ON/OFF LED (D2)
+  digitalWrite(LED_ONOFF_PIN, p7 > 1500 ? HIGH : LOW);
 
-  int bright = constrain(map(pw3, 1000, 2000, 0, 255), 0, 255);
-  if (bright <= 20) digitalWrite(LED_BRIGHT_PIN, LOW);
-  else              analogWrite(LED_BRIGHT_PIN, bright);
+  // 3) 밝기 LED (D3)
+  int brightness = constrain(map(p3, 1000, 2000, 0, 255), 0, 255);
+  if (brightness <= 20) digitalWrite(LED_BRIGHT_PIN, LOW);
+  else                  analogWrite(LED_BRIGHT_PIN, brightness);
 
-  float hue = map(pw2, 1000, 2000, 0, 360);
+  // 4) RGB LED (D4,5,6) 
+  float hue = map(p2, 1000, 2000, 0, 360);
   int r, g, b;
-  hsvToRgb(hue, 1.0, 1.0, r, g, b);
+  hsvToRgb(hue, 1.0f, 1.0f, r, g, b);
   analogWrite(LED_R_PIN, 255 - r);
   analogWrite(LED_G_PIN, 255 - g);
   analogWrite(LED_B_PIN, 255 - b);
 
-  if (u7 || u3 || u2) {
-    Serial.print("CH7="); Serial.print(pw7);
-    Serial.print(" CH3="); Serial.print(pw3);
-    Serial.print(" CH2="); Serial.print(pw2);
-    Serial.print(" Hue="); Serial.print(hue);
-    Serial.print(" BrightLED="); Serial.println(bright);
-  }
-
-  delay(20);
+  delay(20);  
 }
